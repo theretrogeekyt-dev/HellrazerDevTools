dc008cc73db6f9682b3e7c9a90b4f637c7bdba81
theretrogeekyt-dev <joshuamichael8841@gmail.com>
2026-02-06 15:00:50 -0800

    Create wolf3d.c with raycasting rendering implementation for MS-DOS

---
+++ wolf3d.c
@@ -0,0 +1,22 @@
+// wolf3d.c
+// Raycasting rendering implementation for MS-DOS
+
+#include <dos.h>
+#include <stdio.h>
+#include <conio.h>
+
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 200
+
+void render_frame() {
+    // Implementation of rendering logic
+    // Set video mode and render raycasting
+}
+
+void main() {
+    // Initialize graphics
+    render_frame();
+
+    // Wait for key press to exit
+    getch();
+}
